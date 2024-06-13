53c82e0191d4f3dd2ca41a192793a698ce267b92
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-14 02:42:06 +0300

    task 12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,37 +1,35 @@
--- Drop the procedure if it already exists
+-- SQL script that creates a stored procedure
+--ComputeAverageWeightedScoreForUser that computes
+--and store the average weighted score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
-
--- Change delimiter to define the stored procedure
 DELIMITER $$
-
--- Create the ComputeAverageWeightedScoreForUser stored procedure
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE sum_scores DECIMAL(10,2);
-    DECLARE total_weight DECIMAL(10,2);
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
 
-    -- Calculate the sum of scores weighted by project importance
-    SELECT SUM(s.score * p.importance) INTO sum_scores, SUM(p.importance) INTO total_weight
-    FROM scores s
-    JOIN projects p ON s.project_id = p.id
-    WHERE s.user_id = user_id;
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
 
-    -- Calculate the average weighted score
-    DECLARE avg_weighted_score DECIMAL(5,2);
-    IF total_weight > 0 THEN
-        SET avg_weighted_score = sum_scores / total_weight;
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
     ELSE
-        SET avg_weighted_score = 0;
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
     END IF;
-
-    -- Insert or update the average weighted score in the average_weighted_scores table
-    INSERT INTO average_weighted_scores (user_id, avg_weighted_score)
-    VALUES (user_id, avg_weighted_score)
-    ON DUPLICATE KEY UPDATE avg_weighted_score = avg_weighted_score;
 END $$
-
--- Reset delimiter to default
 DELIMITER ;
-
