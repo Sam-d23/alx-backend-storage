7e09e80bbdb1a5697f12503a9640bc48385a651f
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-14 02:16:26 +0300

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,9 +1,2 @@
--- Create the names table if it does not exist
-CREATE TABLE IF NOT EXISTS names (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    score INT
-);
-
 -- Create the index on the first letter of the name column and score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
