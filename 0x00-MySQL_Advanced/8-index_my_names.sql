6296b25ee33109e37d8b7cacc8a63804fd3732f4
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-14 01:57:44 +0300

    task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,8 +1,2 @@
--- Create the names table if it does not exist
-CREATE TABLE IF NOT EXISTS names (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
 -- Create the index on the first letter of the name column
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));`
+CREATE INDEX idx_name_first ON names(name(1));
