190c0ea4cefaed9c72318acd43ee58d93777a17b
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-14 02:09:51 +0300

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,3 @@
 --Ranks country origins of bands
-SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) as nb_fans from metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
