e140d3df597d7d8007d6afd855e8a3295d970002
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-14 23:07:34 +0300

    task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,14 +1,22 @@
--- SQL script to create a function SafeDiv
--- The function divides the first number by the second number
--- and returns 0 if the second number is equal to 0
+-- SQL script that creates a function SafeDiv
+-- This function divides the first argument by the second or returns 0 if the second argument is 0
 
+-- Drop the function if it already exists to avoid errors
+DROP FUNCTION IF EXISTS SafeDiv;
+
+-- Create the SafeDiv function
 DELIMITER $$
 
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 2)
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10, 2)
+DETERMINISTIC
 BEGIN
+    -- Check if the divisor is 0
     IF b = 0 THEN
+        -- Return 0 if the divisor is 0
         RETURN 0;
     ELSE
+        -- Otherwise, return the result of the division
         RETURN a / b;
     END IF;
 END $$
