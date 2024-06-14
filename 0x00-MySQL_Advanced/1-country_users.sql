3ec2251775ffb6ca02875255283e87795d0defdd
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-14 22:41:18 +0300

    task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
---SQL script that contains personal details
---and enumeration of countries.
-CREATE TABLE IF NOT EXISTS users(
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255),
-	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+-- SQL script that contains personal details and enumeration of countries.
+-- Creates a table users with attributes id, email, name, and country
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
