4f0a2be1c3818722572ac121149b125839aabab6
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-18 15:58:41 +0300

    Task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,9 +8,5 @@ def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document in a collection based on kwargs.
     """
-    result = mongo_collection.insert_one(kwargs)
-    return str(result.inserted_id)
-
-
-if __name__ == "__main__":
-    pass
+    new = mongo_collection.insert_one(kwargs)
+    return new.inserted_id
