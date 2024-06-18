80b4be5e825310895bbd534477596959831afc89
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-18 17:26:28 +0300

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,19 +8,24 @@ statistics about the Nginx logs stored in the collection.
 
 from pymongo import MongoClient
 
-def log_stats():
+
+def log_stats(mongo_collection):
     """
-    Connect to MongoDB server
+    The function of the script.
     """
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.logs
-    collection = db.nginx
-    total_logs = collection.count_documents({})
+    total_logs = mongo_collection.count_documents({})
     print(f"{total_logs} logs")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        count = collection.count_documents({"method": method})
+        count = mongo_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    status_check_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check_count} status check")
+
+
+if __name__ == "__main__":
+    # Connect to MongoDB server and access the logs.nginx collection
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    log_stats(nginx_collection)
