8103f64ec63b3e6562935314f5aba5a64656f700
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-19 18:53:25 +0300

    Task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
+
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+redis = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.'''
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        '''Wrapper function for caching the output.'''
-        key_count = f'count:{url}'
-        key_result = f'result:{url}'
+def wrap_requests(fn: Callable) -> Callable:
+    """Wrapper Decorator"""
 
-        redis_store.incr(key_count)
-        cached_result = redis_store.get(key_result)
-        if cached_result:
-            return cached_result.decode('utf-8')
-
-        result = method(url)
-        redis_store.setex(key_result, 10, result)
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper decorator"""
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
         return result
+
     return wrapper
 
 
