da6aabb4a2d2b4884783c28d5c7145fc869f4539
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-19 18:39:53 +0300

    Obtains the HTML content.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,76 +1,34 @@
 #!/usr/bin/env python3
 """
-Module for fetching web pages with caching and counting.
+web cache and tracker
 """
-
-
 import requests
 import redis
-from typing import Callable
 from functools import wraps
-from time import sleep
+
+store = redis.Redis()
 
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = f"cached:{url}"
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-
-def cache_and_count(func: Callable) -> Callable:
-    """
-    Decorator to cache function results
-    in Redis with a count of accesses.
-
-    Args:
-        func (Callable): Function to decorate.
-
-    Returns:
-        Callable: Decorated function.
-    """
-    @wraps(func)
-    def wrapper(url: str) -> str:
-        """
-        Wrapper function to cache and count
-        accesses for the decorated function.
-
-        Args:
-            url (str): URL of the web page to fetch.
-
-        Returns:
-            str: HTML content of the web page.
-        """
-        # Count accesses
-        count_key = f"count:{url}"
-        redis_client.incr(count_key)
-
-        # Cache result with expiration time
-        content_key = f"content:{url}"
-        cached_content = redis_client.get(content_key)
-        if cached_content:
-            return cached_content.decode('utf-8')
-
-        # Fetch content if not cached
-        html_content = func(url)
-        redis_client.setex(content_key, 10, html_content)
-
-        return html_content
+        html = method(url)
+        store.incr(f"count:{url}")
+        store.setex(cached_key, 10, html)
+        return html
 
     return wrapper
 
 
-@cache_and_count
+@count_url_access
 def get_page(url: str) -> str:
-    """
-    Fetches the HTML content of a web page.
+    """ Returns HTML content of a url """
+    return requests.get(url).text
 
-    Args:
-        url (str): URL of the web page to fetch.
-
-    Returns:
-        str: HTML content of the web page.
-    """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.text
-    except requests.RequestException as e:
-        print(f"Error fetching {url}: {e}")
-        return ""
