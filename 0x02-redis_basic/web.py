43824d288332697d0bcb2be4ba2553951a52253d
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-06-20 05:14:35 +0300

    Task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python3
 """
-Returns HTML content.
+Module caches HTTP requests and tracks access.
 """
 
 import redis
 import requests
-from typing import Callable
 from functools import wraps
+from typing import Callable
 
-redis = redis.Redis()
+# Redis client is initialized
+redis_client = redis.Redis()
 
 
-def wrap_requests(fn: Callable) -> Callable:
-    """Wrapper decorates"""
-
+def track_get_page(fn: Callable) -> Callable:
+    """
+    Decorator checks if URL's data is cached
+    and tracks number of times get_page is called
+    """
     @wraps(fn)
-    def wrapper(url):
-        """ Wrapper"""
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
+    def wrapper(url: str) -> str:
+        """
+        Increments access count for the URL
+        and returns available cached data.
+        Fetches and caches data if not available
+        """
+        # Number of times the URL is accessed, is tracked
+        redis_client.incr(f'count:{url}')
+        # Check whether URL's data is cached
+        cached_page = redis_client.get(url)
+        if cached_page:
+            return cached_page.decode('utf-8')
+        # Fetches data and cache it
+        response = fn(url)
+        redis_client.setex(url, 10, response)
+        return response
 
     return wrapper
 
 
-@wrap_requests
+@track_get_page
 def get_page(url: str) -> str:
-    """HTML content is returned of a url"""
+    """
+    HTTP GET request to a given URL is made
+    and the response text is returned.
+    """
     response = requests.get(url)
+    response.raise_for_status()
     return response.text
